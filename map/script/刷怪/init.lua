b8a2d0e68f140186b37e3c75a21ef9175591f534
DESKTOP-1LO98I2\Mloong <418796833@qq.com>
2019-04-10 18:54:12 +0800

    1.938

--- map/script/刷怪/init.lua
+++ map/script/刷怪/init.lua
@@ -1,12 +1,3 @@
-
--- require '刷怪.BOSS技能'
--- require '刷怪.技能'
--- require '刷怪.刷兵规则'
--- require '刷怪.掉落'
--- require '刷怪.BOSS-AI'
--- require '刷怪.BOSS光环'
--- require '刷怪.领主'
--- require '刷怪.领主-AI'
 require '刷怪.creep'
 require '刷怪.刷怪'
 require '刷怪.最终boss'
@@ -40,13 +31,14 @@ require '刷怪.钥匙怪奖励'
 -- end
 -- mt:start()
 
---如下代码可以释放 内存
+-- 如下代码可以释放 内存
 -- ac.loop(10*1000,function()
---     for i=1,40 do 
---         local unit = ac.player.com[2]:create_unit('强盗',ac.point(0,0))
---         unit:set('移动速度',400)
---         unit:set_search_range(99999)
---     end    
+    -- for i=1,40 do 
+    --     local unit = ac.player.com[2]:create_unit('强盗',ac.point(0,0))
+    --     unit:set('生命上限',10000000000)
+    --     unit:set('移动速度',400)
+    --     unit:set_search_range(99999)
+    -- end    
 -- end)
 
 
